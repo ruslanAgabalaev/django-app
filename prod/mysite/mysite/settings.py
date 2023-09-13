749341a1d7d3b72791e6939b6114386e4f23c341
Ruslan <88150716+ruslanAgabalaev@users.noreply.github.com>
2023-09-13 17:43:57 +0300

    configger for deploy

--- prod/mysite/mysite/settings.py
+++ prod/mysite/mysite/settings.py
@@ -182,7 +182,7 @@ logging.config.dictConfig({
     "disable_existing_loggers": False,
     "formatters": {
         'console': {
-            "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(message)s"
+            "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(message)s",
         },
     },
     "handlers": {
